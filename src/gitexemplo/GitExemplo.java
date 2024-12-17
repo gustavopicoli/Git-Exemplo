d30549239d5d79e1b6e36bc3a31177227de2dad0
gustavopicoli <52050205+gustavopicoli@users.noreply.github.com>
2024-12-17 14:57:45 -0300

    Olá mundo

--- src/gitexemplo/GitExemplo.java
+++ src/gitexemplo/GitExemplo.java
@@ -1,8 +1,4 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
- */
-package gitexemplo;
+ package gitexemplo;
 
 /**
  *
@@ -14,7 +10,7 @@ public class GitExemplo {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        System.out.println("Olá, mundo!");
     }
     
 }
